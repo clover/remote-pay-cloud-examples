0fa46cf9dff53253b50b57945d8f0b7711ecc28c
rayramsay <rachel@clover.com>
2018-01-25 09:05:50 -0800

    Update config.js

--- remote-pay-cloud-pos-react/src/config.js
+++ remote-pay-cloud-pos-react/src/config.js
@@ -1,6 +1,6 @@
 export const myConfig = {
     clientId: 'yourClientIdHere',
-    devicesDomain: 'https://sandbox.dev.clover.com/v3/merchants/',
+    devicesDomain: 'https://apisandbox.dev.clover.com/v3/merchants/',
     oAuthDomain: 'https://sandbox.dev.clover.com',
     uriText: 'wss://10.249.254.214:12345/remote_pay'
 };
