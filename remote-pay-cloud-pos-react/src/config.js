b4370b7b9cd3581824b36c2bdf459541bbd3d6fd
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2019-05-15 15:22:29 -0400

    adds missing configuration property to `react-pay-cloud-pos-react` example

--- remote-pay-cloud-pos-react/src/config.js
+++ remote-pay-cloud-pos-react/src/config.js
@@ -3,4 +3,5 @@ export const myConfig = {
     devicesDomain: 'https://apisandbox.dev.clover.com/v3/merchants/',
     oAuthDomain: 'https://sandbox.dev.clover.com',
     uriText: 'wss://10.249.255.103:12345/remote_pay',
+    cloverServer: 'https://apisandbox.dev.clover.com',
 };
