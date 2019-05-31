79af5b59c3a2420cfa0f82eac00a0ff180eb79e6
david-clover-com <david.marginian@clover.com>
2019-05-31 14:25:14 -0600

    3.1.0

--- remote-pay-cloud-pos-react/src/config.js
+++ remote-pay-cloud-pos-react/src/config.js
@@ -1,6 +1,6 @@
 export const myConfig = {
-    clientId: 'QD50YZNTAW3KW',
-    devicesDomain: 'http://localhost:9000/v3/merchants/',
-    oAuthDomain: 'http://localhost:9000',
+    clientId: 'HBK8YZG9EQNJG',
+    devicesDomain: 'https://apisandbox.dev.clover.com/v3/merchants/',
+    oAuthDomain: 'https://sandbox.dev.clover.com',
     uriText: 'wss://10.249.255.103:12345/remote_pay',
 };
